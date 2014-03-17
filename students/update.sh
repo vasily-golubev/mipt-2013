dba97b6a7187aa703ab46e78d7193d4b2d0847a7
Vasily Golubev <w.golubev@mail.ru>
2014-03-17 17:09:51 +0400

    Fixed update script.

--- students/update.sh
+++ students/update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd ..
+git submodule update
+cd "students"
 for dir in */ ; do
 	cd $dir
 	# Pull latest data from student's repo.
+	git submodule update
 	git merge origin/master
 	cd ..
 done
