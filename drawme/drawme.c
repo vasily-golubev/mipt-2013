61f023012015f1360d2a9109c00e6daf3efc5ae3
Vasily Golubev <w.golubev@mail.ru>
2014-04-21 19:34:24 +0400

    [drawme] imporved.

--- drawme/drawme.c
+++ drawme/drawme.c
@@ -23,3 +23,30 @@ int DM_plot_1d(double *array_x, double *array_y, unsigned int len, char *title, 
 
 	return 0;
 }
+
+int DM_plot_1d_etalon(double *array_x, double *array_y, double *array_etalon,
+		unsigned int len, char *title, char screen) {
+	gnuplot_ctrl *h;
+	h = gnuplot_init();
+
+	// Plot in PNG file.
+	if (!screen) {
+		gnuplot_cmd(h, "set terminal png");
+		// FIXME Now rewrite current image.
+		gnuplot_cmd(h, "set output \"result.png\"");
+	}
+	gnuplot_setstyle(h, "lines");
+	gnuplot_cmd(h, "set xrange [-1:1]");
+	gnuplot_cmd(h, "set yrange [0:2]");
+	gnuplot_set_xlabel(h, "Coordinate, x");
+	gnuplot_set_ylabel(h, "Value, u");
+	gnuplot_cmd(h, "set multiplot");
+	gnuplot_plot_xy(h, array_x, array_y, len, title);
+	gnuplot_plot_xy(h, array_x, array_etalon, len, "Etalon");
+	gnuplot_cmd(h, "unset multiplot");
+	// FIXME Remove me.
+	sleep(2);
+	gnuplot_close(h);
+
+	return 0;
+}
--- drawme/drawme.h
+++ drawme/drawme.h
@@ -1,2 +1,4 @@
 // Plot 1D array of doubles.
 int DM_plot_1d(double *array_x, double *array_y, unsigned int len, char *title, char screen);
+// Plot 1D array of doubles and etalon curve.
+int DM_plot_1d_etalon(double *array_x, double *array_y, unsigned int len, char *title, char screen);
